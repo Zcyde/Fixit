f1fff9620e76009102ee2d67c41054e1b2544751
LozanoHAU <l0zd4n3.aa@gmail.com>
2026-03-11 19:54:51 +0800

    📝 Optimized for FM1 📝

--- lib/sign_up_page.dart
+++ lib/sign_up_page.dart
@@ -84,7 +84,7 @@ class _SignUpPageState extends State<SignUpPage> {
                 border: Border.all(color: Colors.black, width: 2),
                 boxShadow: [
                   BoxShadow(
-                    color: Colors.black.withOpacity(0.1),
+                    color: Colors.black.withValues(alpha: 0.1),
                     blurRadius: 10,
                     offset: const Offset(0, 4),
                   ),
