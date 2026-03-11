598757b6ae87e39d66c7a2a18e42f45e9061c052
LozanoHAU <l0zd4n3.aa@gmail.com>
2026-03-11 19:53:33 +0800

    📝 Optimized for FM1 📝

--- lib/sign_in_page.dart
+++ lib/sign_in_page.dart
@@ -74,7 +74,7 @@ class _SignInPageState extends State<SignInPage> {
                 border: Border.all(color: Colors.black, width: 2),
                 boxShadow: [
                   BoxShadow(
-                    color: Colors.black.withOpacity(0.1),
+                    color: Colors.black.withValues(alpha: 0.1),
                     blurRadius: 10,
                     offset: const Offset(0, 4),
                   ),
